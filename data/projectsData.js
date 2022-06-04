9c03900d81d100c9aa932171dca6c2c5bb78f44c
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-06-04 10:40:15 -0500

    Add shopify app review scraper project

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,8 +2,14 @@ const projectsData = [
   {
     title: 'AlgebraSource.com',
     description: `An Algebra resource website built with NextJS and TailwindCSS.`,
-    imgSrc: '/static/images/swomath-logo.png',
+    imgSrc: '/static/images/algebra-source-project.png',
     href: 'https://algebrasource.com',
+  },
+  {
+    title: 'Shopify App Review Scraper',
+    description: `A simple Java project using Spring Boot that successfully scraped all 650,000+ reviews from over 7,000 apps from Shopify's App Store and stored them in an AWS RDS PostgreSQL database.`,
+    imgSrc: '/static/images/shopify-app-review-scraper-project.png',
+    href: 'https://github.com/tomswokowski/shopify-app-review-scraper',
   },
 ]
 

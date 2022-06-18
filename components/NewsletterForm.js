c3afa742e5a7b4c5ba98f26c2ff10691b588d3c6
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-06-18 12:01:58 -0500

    fix newsletter width

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -48,7 +48,7 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
           </label>
           <input
             autoComplete="email"
-            className="w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black"
+            className="w-full rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black"
             id="email-input"
             name="email"
             placeholder={subscribed ? "You're subscribed !  🎉" : 'Enter your email...'}
@@ -60,7 +60,7 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
         </div>
         <div className="mt-2 rounded-md shadow-sm">
           <button
-            className={`w-72 rounded-md bg-primary-500 py-2 px-4 font-medium text-white ${
+            className={`w-full rounded-md bg-primary-500 py-2 px-4 font-medium text-white ${
               subscribed ? 'cursor-default' : 'hover:bg-primary-700 dark:hover:bg-primary-400'
             } focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black`}
             type="submit"
